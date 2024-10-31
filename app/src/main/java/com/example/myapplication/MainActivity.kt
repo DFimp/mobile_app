70d6e4ce565578cd8c7bcaabf239835dd16a6f1a
DFimp <ilia.fimp@gmail.com>
2024-10-31 17:47:16 +0300

    laba_3.2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,22 +12,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Создание объектов
         val eng = Engineer(101, "Jane Smith", "012-34-5678", 120_345.27)
         val mgr = Manager(207, "Barbara Johnson", "054-12-2367", 109_501.36, "US Marketing")
         val adm = Admin(304, "Bill Munroe", "108-23-2367", 75_002.34)
         val dir = Director(12, "Susan Wheeler", "099-45-2340", 120_567.36, "Global Marketing", 1_000_000.00)
 
+        // Вывод информации о сотрудниках
         printEmployee(eng)
         printEmployee(mgr)
         printEmployee(adm)
         printEmployee(dir)
     }
 
+    // Метод для отображения данных о сотруднике
     private fun printEmployee(emp: Employee) {
-        Log.d("EmployeeTest", "Employee ID: ${emp.empId}")
-        Log.d("EmployeeTest", "Employee Name: ${emp.name}")
-        Log.d("EmployeeTest", "Employee Soc Sec #: ${emp.ssn}")
-        Log.d("EmployeeTest", "Employee salary: ${emp.salary}")
+        Log.d("EmployeeTest", "ID сотрудника: ${emp.empId}")
+        Log.d("EmployeeTest", "Имя сотрудника: ${emp.name}")
+        Log.d("EmployeeTest", "Социальный номер: ${emp.ssn}")
+        Log.d("EmployeeTest", "Зарплата сотрудника: ${emp.salary}")
     }
 }
 
