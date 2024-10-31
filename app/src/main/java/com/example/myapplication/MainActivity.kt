d733d9aa87a641fc535ed5f09b799307848501f2
DFimp <ilia.fimp@gmail.com>
2024-10-31 16:07:51 +0300

    laba_3.1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,63 +1,33 @@
 package com.example.myapplication
-
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.domain.Admin
+import com.example.myapplication.domain.Director
+import com.example.myapplication.domain.Engineer
+import com.example.myapplication.domain.Manager
+import com.example.myapplication.domain.Employee
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val eng = Engineer(101, "Jane Smith", "012-34-5678", 120_345.27)
+        val mgr = Manager(207, "Barbara Johnson", "054-12-2367", 109_501.36, "US Marketing")
+        val adm = Admin(304, "Bill Munroe", "108-23-2367", 75_002.34)
+        val dir = Director(12, "Susan Wheeler", "099-45-2340", 120_567.36, "Global Marketing", 1_000_000.00)
 
-        // Инициализация элементов интерфейса
-        val editTextArea = findViewById<EditText>(R.id.editTextArea)
-        val editTextServicePrice = findViewById<EditText>(R.id.editTextServicePrice)
-        val checkBoxHeating = findViewById<CheckBox>(R.id.checkBoxHeating)
-        val checkBoxWater = findViewById<CheckBox>(R.id.checkBoxWater)
-        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+        printEmployee(eng)
+        printEmployee(mgr)
+        printEmployee(adm)
+        printEmployee(dir)
+    }
 
-        val buttonCalculate = findViewById<Button>(R.id.buttonCalculate)
-        val buttonReset = findViewById<ImageButton>(R.id.buttonReset)
-
-        // Обработка нажатия на кнопку "Рассчитать"
-        buttonCalculate.setOnClickListener {
-            val area = editTextArea.text.toString().toDoubleOrNull() ?: 0.0
-            val servicePrice = editTextServicePrice.text.toString().toDoubleOrNull() ?: 0.0
-
-            var totalCost = area * servicePrice
-
-            if (checkBoxHeating.isChecked) {
-                totalCost += 500  // Пример стоимости отопления
-            }
-
-            if (checkBoxWater.isChecked) {
-                totalCost += 300  // Пример стоимости водоснабжения
-            }
-
-            textViewResult.text = "Стоимость: $totalCost руб."
-        }
-
-        // Обработка нажатия на кнопку "Сброс"
-        buttonReset.setOnClickListener {
-            editTextArea.text.clear()
-            editTextServicePrice.text.clear()
-            checkBoxHeating.isChecked = false
-            checkBoxWater.isChecked = false
-            textViewResult.text = "Стоимость: 0 руб."
-        }
+    private fun printEmployee(emp: Employee) {
+        Log.d("EmployeeTest", "Employee ID: ${emp.empId}")
+        Log.d("EmployeeTest", "Employee Name: ${emp.name}")
+        Log.d("EmployeeTest", "Employee Soc Sec #: ${emp.ssn}")
+        Log.d("EmployeeTest", "Employee salary: ${emp.salary}")
     }
 }
+
